74f85962bd27a92d01de28e407af4c5c297b1a3d
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-03-10 16:20:58 +0400

    add contact

--- js/script.js
+++ js/script.js
@@ -4,17 +4,4 @@ $("#burger-menu").on("click", function(){
     $("#socicon").toggleClass("open-socicon");
   })
 
-  $('.slider-cont').slick({
-    prevArrow: $('.arrow-prev'),
-    nextArrow: $('.arrow-next'),
-  });
 
-  $('.cont').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: $('.arrow-prev-2'),
-    nextArrow: $('.arrow-next-2'),
-  });
-      
