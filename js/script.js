7b944e78daa68a989c1e7ad334d43a6a1e0c16df
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-10-15 17:24:28 +0400

    add sid bar

--- js/script.js
+++ js/script.js
@@ -16,6 +16,28 @@ window.onscroll = function(){
     header.classList.remove('active');
   }
 };
+//------- MENU FILTR  -----------
+$(document).ready(function() {
+  $('.sub-btn').click(function() {
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+  });
+  $('.flt-icon').click(function() {
+    $('.side-bar').addClass('bar-active');
+    $('.close-btn').css("visibility", "visible");
+    $('.flt-ico-nm-bar').css("visibility", "visible");
+  });
+  $('.sid-icon').click(function() {
+    $('.side-bar').addClass('bar-active');
+    $('.close-btn').css("visibility", "visible");
+    $('.flt-ico-nm-bar').css("visibility", "visible");
+  });
+  $('.close-btn').click(function() {
+    $('.side-bar').removeClass('bar-active');
+    $('.close-btn').css("visibility", "hidden");
+    $('.flt-ico-nm-bar').css("visibility", "hidden");
+  });
+});
 //------- DEMO BANER ------------
  const demoBanner = document.querySelector(".demo-cont")
  const demoBtn = document.querySelector(".demo-cont button")
