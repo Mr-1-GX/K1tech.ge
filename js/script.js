592432e72db09d2cc2256cc02e1c445cda378f5e
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2021-10-21 17:55:42 +0400

    big change

--- js/script.js
+++ js/script.js
@@ -20,22 +20,45 @@ window.onscroll = function(){
 $(document).ready(function() {
   $('.sub-btn').click(function() {
     $(this).next('.sub-menu').slideToggle();
+    $(this).next('.sub-menu-2').slideToggle();
+    $(this).next('.sub-menu-3').slideToggle();
+    $(this).next('.sub-menu-4').slideToggle();
     $(this).find('.dropdown').toggleClass('rotate');
   });
   $('.flt-icon').click(function() {
     $('.side-bar').addClass('bar-active');
     $('.close-btn').css("visibility", "visible");
     $('.flt-ico-nm-bar').css("visibility", "visible");
+    $('.sid-icon').css("visibility", "hidden");
+  });
+  $('.user-icon').click(function() {
+    $('.user-bar').addClass('user-bar-active');
+    $('.user-close-btn').css("visibility", "visible");
+    $('.user-ico-nm-bar').css("visibility", "visible");
   });
   $('.sid-icon').click(function() {
     $('.side-bar').addClass('bar-active');
     $('.close-btn').css("visibility", "visible");
     $('.flt-ico-nm-bar').css("visibility", "visible");
+    $('.sid-icon').css("visibility", "hidden");
+  });
+  $('.users-icon').click(function() {
+    $('.user-bar').addClass('user-bar-active');
+    $('.user-close-btn').css("visibility", "visible");
+    $('.user-ico-nm-bar').css("visibility", "visible");
+    $('.users-icon').css("visibility", "hidden");
   });
   $('.close-btn').click(function() {
     $('.side-bar').removeClass('bar-active');
     $('.close-btn').css("visibility", "hidden");
     $('.flt-ico-nm-bar').css("visibility", "hidden");
+    $('.sid-icon').css("visibility", "visible");
+  });
+  $('.user-close-btn').click(function() {
+    $('.user-bar').removeClass('user-bar-active');
+    $('.user-close-btn').css("visibility", "hidden");
+    $('.user-ico-nm-bar').css("visibility", "hidden");
+    $('.users-icon').css("visibility", "visible");
   });
 });
 //------- DEMO BANER ------------
